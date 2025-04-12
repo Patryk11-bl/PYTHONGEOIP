e3c0e61fa6d4a48262068a645495f63c900dbba7
Patryk11-bl <bloch.patryk.pb1@gmail.com>
2025-04-12 16:03:42 +0200

    first commit

---
+++ import geocoder.py
@@ -0,0 +1,19 @@
+import geocoder
+import folium
+
+def get_localization(ip_addr):
+    location = geocoder.ip(ip_addr)
+    return location
+
+myip  = input('Wprowadź adres ip do zlokalizowania')
+location_info = get_localization(myip)
+
+print('country', location_info.country)
+print('city', location_info.city)
+print('latitude, longitude', location_info.latng[0], location_info.latlng[1])
+
+coordinate = [location_info.latlng[0], location_info.latlng[1]]
+myloc = folium.Map(location=coordinate, zoom_start=14, popup='My Location')
+folium.Marker(coordinate, icon=folium.Icon(color='blue', icon_color='blue', prefix='fa',icon='male')).add_to(myloc)
+folium.Circle(location=coordinate, radius=200).add_to(myloc)
+myloc.save('mymap.html')
