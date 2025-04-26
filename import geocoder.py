9bd1771eb439ebdf48a209eeef21b38c073e71a0
Patryk11-bl <bloch.patryk.pb1@gmail.com>
2025-04-26 14:44:05 +0200

    lekcja 26

--- import geocoder.py
+++ import geocoder.py
@@ -17,3 +17,4 @@ myloc = folium.Map(location=coordinate, zoom_start=14, popup='My Location')
 folium.Marker(coordinate, icon=folium.Icon(color='blue', icon_color='blue', prefix='fa',icon='male')).add_to(myloc)
 folium.Circle(location=coordinate, radius=200).add_to(myloc)
 myloc.save('mymap.html')
+
