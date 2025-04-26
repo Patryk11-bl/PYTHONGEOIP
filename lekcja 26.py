e528900501dc9f2141dd58a5c16b6674629def9f
Patryk11-bl <bloch.patryk.pb1@gmail.com>
2025-04-26 15:06:00 +0200

    koniec

--- lekcja 26.py
+++ lekcja 26.py
@@ -1,1 +1,18 @@
-oceny = [1,2,3,4,5,4,5,6,5,6]
+import copy
+oceny = [1,2,3,4,5,6,1,2,3,4,5,6,5,4,3,2,1,6]
+nowe_oceny = []
+
+for e in oceny:
+    if e == 4:
+        e=3
+    elif e ==2:
+        e =1
+    elif e ==5:
+        e= 3
+        
+        
+    elif e == 6:
+        e =4
+    nowe_oceny.append(e)
+
+print(nowe_oceny)
